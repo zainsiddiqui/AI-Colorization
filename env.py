e42c04ae45dabb30e92a703a1ea4f1fc83956eff
krupee <krupal.patel21@gmail.com>
2020-04-30 17:51:52 -0400

    up

---
+++ env.py
@@ -0,0 +1,16 @@
+# Environment
+
+from PIL import Image
+import numpy as np
+
+class Picture:
+    def __init__(self, image_file):
+        
+        im = Image.open(image_file)
+        pic = im.load()
+        self.x_length = im.size[0]
+        self.y_length = im.size[1]
+
+Picture("beach.jpg")
+
+        
